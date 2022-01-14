588bf2be382910888121bcb471152dd030c36d68
Alex Schembri <alexschembri@outlook.com>
2022-01-14 14:45:32 +0000

    Removed apicache dependency

--- src/routes/results.ts
+++ src/routes/results.ts
@@ -1,9 +1,7 @@
 import { Request, Response, Router } from 'express'
-import apicache from 'apicache'
 
 const routes = Router();
 
-let cache = apicache.middleware
 routes.get('/', (req:Request, res:Response )=>res.json( Object.keys(req) ))
 
 module.exports = routes

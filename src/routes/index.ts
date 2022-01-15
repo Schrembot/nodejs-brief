1bb3444513679c1a0cca5b9fa55a3924f0e10469
Alex Schembri <alexschembri@outlook.com>
2022-01-15 22:49:08 +0000

    Made / route a simple 200 response

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ const routes = Router()
 routes.use('/teams', require('./teams'))
 routes.use('/players', require('./players'))
 routes.use('/results', require('./results'))
-routes.get('/', (req: Request, res: Response)  => res.send('Ready'))
+routes.get('/', (req: Request, res: Response)  => res.sendStatus(200))
 routes.all('*', (req: Request, res: Response)  => res.sendStatus(404))
 
 module.exports = routes
